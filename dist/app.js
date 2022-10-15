6e117dadb7b524a6f0b4d89abe821e98cfd5b7f6
Gustavo Gomes <gustavohenriqueny@gmail.com>
2022-10-15 11:16:02 -0300

    Fim da aula 9 do curso de angular, usando funções em typescript.

--- dist/app.js
+++ dist/app.js
@@ -7,3 +7,16 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = "BB-8";
 console.log("My favorite droid is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsects) {
+    return parsects < 12;
+};
+var distance = 11;
+console.log("Is ".concat(distance, " parsecs enoughs to beat Millennium Falcon ? ").concat(isEnoughToBeatMF(distance) ? "YES" : "NO"));
+var call = function (name) { return console.log("Do you copy, ".concat(name, "?")); };
+call("R2-D2");
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log(inc(5, 1));
+console.log(inc(5));
